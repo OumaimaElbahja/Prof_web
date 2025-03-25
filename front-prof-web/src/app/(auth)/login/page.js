f7ebb6a207d8fee4bd3a4a5dc234d9316e1bba3d
Noureddine DRIOUECH <nourddinedriouech@gmail.com>
2025-03-25 22:26:32 +0000

    change the login image

--- front-prof-web/src/app/(auth)/login/page.js
+++ front-prof-web/src/app/(auth)/login/page.js
@@ -4,20 +4,19 @@ import { LoginForm } from "./components/login-form";
 export default function LoginPage() {
   return (
     <>
-<div className="grid grid-cols-2 h-screen">
-  <div className="relative">
-    <Image
-      src="/login.jpg"
-      alt="Login Icon"
-      className="object-cover w-full h-full dark:brightness-[0.2] dark:grayscale"
-      fill
-    />
-  </div>
-  <div className="flex items-center justify-center p-8">
-    <LoginForm />
-  </div>
-</div>
-
+      <div className="grid grid-cols-2 h-screen">
+        <div className="relative bg-secondary">
+          <Image
+            src="/loginImage.svg"
+            alt="Login Icon"
+            className="object-cover scale-75 w-full h-full dark:brightness-[0.2] dark:grayscale"
+            fill
+          />
+        </div>
+        <div className="flex items-center justify-center  p-8">
+          <LoginForm />
+        </div>
+      </div>
     </>
   );
 }
